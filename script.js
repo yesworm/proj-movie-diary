48cb1038373dd83bd397d3b85edff407e0721e1c
sam geyer <geyerwork@gmail.com>
2024-10-25 15:07:22 +1100

    updated base styling and beginning of html structure

---
+++ script.js
@@ -0,0 +1,11 @@
+const movieData = {
+    "October 2024": [
+      {
+        title: "Oppenheimer",
+        poster: "https://path-to-poster/oppenheimer.jpg",
+        rating: 4.5,
+        letterboxdUrl: "https://letterboxd.com/film/oppenheimer/"
+      },
+      // We'll add more movies here
+    ]
+  };
